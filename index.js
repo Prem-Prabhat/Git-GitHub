0a1711076b13a4c8c06be5abaf0dac8514f56b17
Prem-Prabhat <premprabhat960838@gmail.com>
2025-01-06 14:47:39 +0530

    Add Inital Code

---
+++ index.js
@@ -0,0 +1,8 @@
+function greet(name) {
+    return "Hello, " + name + "!";
+}
+
+let userName = "World";
+let message = greet(userName);
+
+console.log(message);
---
+++ utils/util.js
@@ -0,0 +1,10 @@
+function add(a, b) {
+    return a + b;
+  }
+  
+  let num1 = 5;
+  let num2 = 3;
+  
+  let sum = add(num1, num2);
+  
+  console.log("The sum of", num1, "and", num2, "is", sum);
