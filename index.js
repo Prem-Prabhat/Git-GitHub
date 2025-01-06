811cb66b566918246058bfccea01c2f453d06921
Prem-Prabhat <premprabhat960838@gmail.com>
2025-01-06 15:02:57 +0530

    Add canVote Function

--- index.js
+++ index.js
@@ -6,3 +6,13 @@ let userName = "World";
 let message = greet(userName);
 
 console.log(message);
+
+function canVote(age) {
+    if (age >= 18) {
+      return "You can vote"; 
+    } else {
+      return "You must be at least 18 years old to vote.";
+    }
+  }
+let vote = canVote(20)
+console.log (vote)
